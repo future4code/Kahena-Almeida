ad7c74019ce8b34cb69ab0c1eaebc2b52238e833
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2021-10-07 23:55:20 -0400

    Exercicios aula de funcoes

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,105 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO
+//EXERCÍCIO 01
+
+//1.a. 10, 50
+//1.b. nada
+
+//EXERCÍCIO 02
+
+//2.a. Essa função coloca tudo em letra minúscula, para que seja buscado a palavra cenoura e retorna true/false.
+
+//2.b. Todas serão verdadeiras.
+
+//EXERCÍCIOS DE ESCRITA
+
+//1.a.
+//const sobreMim = ("Eu sou Kahena, tenho 27 anos, moro em Campo Grande/MS e sou estudante de programação.")
+//console.log(sobreMim)
+
+//1.b.
+
+// function dadosSobreMim(nome, idade, cidade, profissão) {
+//     const frase = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}`
+//     return frase
+// }
+
+// const fraseTeste = dadosSobreMim("Kahena", 27, "Campo Grande", "estudante")
+// console.log(fraseTeste)
+
+//2.a.
+
+// function numeros(a, b) {
+//     const soma = Number(a) + Number(b)
+//     return soma
+// }
+
+// const resultado = numeros(10, 20)
+// console.log(resultado)
+
+//2.b.
+
+// function maiorMenor(a, b) {
+//     const bool = Number(a) >= Number(b)
+//     return bool
+// }
+
+// const resultado = maiorMenor(10, 20)
+// console.log(resultado)
+
+//2.c.
+
+// function recebe(a) {
+//     const parImpar = Number(a) / 2
+//     return parImpar
+// }
+
+// var resultado = recebe(2) === 1
+// console.log(resultado)
+// true = par
+
+//2.d.
+
+// function mensagem(texto) {
+//     const maiuscula = texto.toUpperCase()
+//     const tamanho = texto.length
+//     console.log(maiuscula, tamanho)
+// }
+
+// mensagem("string")
+
+//3.
+
+// function soma(x, y) {
+//     const resultado1 =  Number(x) + Number(y)
+//     return resultado1
+// }
+
+// function subtracao(x, y) {
+//     const resultado2 = Number(x) - Number(y)
+//     return resultado2
+// }
+
+// function multiplicacao(x, y) {
+//     const resultado3 = Number(x) * Number(y)
+//     return resultado3
+// }
+
+// function divisao(x, y) {
+//     const resultado4 = Number(x) / Number(y)
+//     return resultado4
+// }
+
+// const numero1 = prompt("Digite um número!")
+// const numero2 = prompt("Digite outro número!")
+
+// const resultadoSoma = soma(numero1, numero2)
+// const resultadoSubtracao = subtracao(numero1, numero2)
+// const resultadoMultiplicacao = multiplicacao(numero1, numero2)
+// const resultadoDivisao = divisao(numero1, numero2)
+
+// console.log(resultadoSoma, "soma")
+// console.log(resultadoSubtracao, "subração")
+// console.log(resultadoMultiplicacao, "multiplicação")
+// console.log(resultadoDivisao, "divisão")
+
+
