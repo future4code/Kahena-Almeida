922cc558f178ab08eec14ef478fff7d4326cb452
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2021-10-08 20:54:13 -0400

    Projeto lista de exercícios JS

---
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,121 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo(altura, largura) {
+  let lado1 = prompt('Qual a altura do retângulo?')
+  let lado2 = prompt('Qual a largura do retângulo?')
+  const area = Number(lado1) * Number(lado2)
+  console.log(area)
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  const anoAtual = prompt('Qual o ano atual?')
+  const anoNascimento = prompt('Qual seu ano de nascimento?')
+  const idade = Number(anoAtual) - Number(anoNascimento)
+  console.log(idade)
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+   const calculo = peso / (altura * altura)
+  return calculo
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  const nome = prompt('Qual seu nome?')
+  const idade = prompt('Qual sua idade?')
+  const email = prompt('Qual seu e-mail?')
+  const frase = `Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`
+  console.log(frase)
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  const cor1 = prompt('Qual sua cor favorita?')
+  const cor2 = prompt('E a segunda favorita?')
+  const cor3 = prompt('E a terceira favorita?')
+  const listaCores = [cor1, cor2, cor3]
+  console.log(listaCores)
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  const maiuscula = string.toUpperCase()
+  return maiuscula
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  const quantidadeMinimaDeIngressos = custo / valorIngresso
+  return quantidadeMinimaDeIngressos
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  const tamanho = string1.length === string2.length
+  return tamanho
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  const primeiro = array[0]
+  return primeiro
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  const ultimo = array[array.length -1]
+  return ultimo
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  let primeiro = array.shift()
+  let ultimo = array.pop()
+  array.push(primeiro)
+  array.unshift(ultimo)
+  return array
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  const bool = (string1.toUpperCase()) === (string2.toUpperCase())
+  return bool
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
