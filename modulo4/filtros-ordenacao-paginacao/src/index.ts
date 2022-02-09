b53332dc3ed55e6cd9d940e5e45043aa85d5ad2c
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-02-09 19:16:30 -0400

    pull

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -1,4 +1,4 @@
-import app from "./app";
+import {app} from "./app";
 import { getAllUsersOrdered } from "./endpoints/getAllUsersOrdered";
 import { getUsersFilterByName } from "./endpoints/getUsersFilterByName";
 import { getUsersFilterByType } from './endpoints/getUsersFilterByType';
