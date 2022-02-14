6fe4670ec43e39eeca6b7303fa3b85df2765f530
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-02-14 19:52:36 -0400

    Segundo

--- modulo5/classes-encapsulamento/src/index.ts
+++ modulo5/classes-encapsulamento/src/index.ts
@@ -28,3 +28,21 @@ const usuario = new UserAccount('123.456.789-30', 'Kahena', 28)
 // c) *Como conseguimos ter acesso às propriedades privadas de uma classe?*
 
 //Através da keyword 'this'.
+
+// type Transaction = {
+//   description: string,
+//   value: number,
+//   date: string
+// }
+
+class Transaction {
+  private date: string;
+  private value: number;
+  private description: string;
+  
+  constructor(date: string, value: number, description: string) {
+    this.date = date;
+    this.value = value;
+    this.description = description
+  }
+}
