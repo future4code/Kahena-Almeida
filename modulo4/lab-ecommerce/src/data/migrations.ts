61050808269b34175b386692bf6948bec3625789
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-02-14 07:32:04 -0400

    Create table products

--- modulo4/lab-ecommerce/src/data/migrations.ts
+++ modulo4/lab-ecommerce/src/data/migrations.ts
@@ -1,7 +1,33 @@
 import connection from "./connection";
+import users from "./users.json";
 import products from "./products.json";
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) };
+
+const createTableUsers = () => connection
+   .raw(`
+      CREATE TABLE IF NOT EXISTS labecommerce_users(
+         id INT PRIMARY KEY,
+         name VARCHAR(255) NOT NULL,
+         email VARCHAR(255) UNIQUE NOT NULL,
+         password VARCHAR(255) NOT NULL
+      );
+   `)
+   .then(() => { console.log("Tabela criada!") })
+   .catch(printError);
+
+const insertUsers = () => connection("labecommerce_users")
+   .insert(users)
+   .then(() => { console.log("Tabela de usuários criada!") })
+   .catch(printError);
+
+const closeConnection = () => { connection.destroy() }
+
+createTableUsers()
+.then(insertUsers)
+.finally(closeConnection);
+
+
 
 const createTableProducts = () => connection
    .raw(`
@@ -20,7 +46,6 @@ const insertProducts = () => connection("labecommerce_products")
    .then(() => { console.log("Produtos inseridos!") })
    .catch(printError);
 
-const closeConnection = () => { connection.destroy() }
 
 createTableProducts()
    .then(insertProducts)
--- modulo4/lab-ecommerce/src/index.ts
+++ modulo4/lab-ecommerce/src/index.ts
@@ -1,8 +1,8 @@
 import app from "./app";
 import createUsers from "./endpoints/createUsers";
 import getAllUsers from "./endpoints/getUsers";
-import createProducts from "./endpoints/createProducts";
+import createProduct from "./endpoints/createProduct";
 
 app.post('/users/create', createUsers);
 app.get('/users', getAllUsers);
-app.post('/products/create', createProducts);
+app.post('/products/create', createProduct);
