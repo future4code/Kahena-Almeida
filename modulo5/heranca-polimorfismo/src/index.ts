bfbf23e9a08ea9cd1caefc57672da8f2a838fb02
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-02-15 12:15:42 -0400

    Exercício 02

--- modulo5/heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -30,8 +30,42 @@ class User {
 	}
 }
 
-const user1 = new User("123","matilda@gmail.com","Matilda","12345")
+const user1: User = new User("123","matilda@gmail.com","Matilda","12345")
 console.log("Exercício 01")
 console.log("ID: ", user1.getId())
 console.log("Nome: ", user1.getName())
 console.log("Email: ", user1.getEmail())
+
+// a) Seria possível imprimir a senha (password) atrelada a essa instância?
+// Não.
+//b) Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+// Uma única vez.
+
+
+// EXERCÍCIO 02
+class Customer extends User {
+  public purchaseTotal: number = 0;
+  private creditCard: string;
+
+  constructor(
+    id: string,
+    email: string,
+    name: string,
+    password: string,
+    creditCard: string
+  ) {
+    super(id, email, name, password);
+    console.log("Chamando o construtor da classe Customer");
+    this.creditCard = creditCard;
+  }
+
+  public getCreditCard(): string {
+    return this.creditCard;
+  }
+}
+
+const customer1: Customer = new Customer("124","mafalda@gmail.com","Mafalda","67890","12343454565453")
+
+//2.a. Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal? Uma vez.
+
+//2.b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê? Duas vezes. Pq a classe customer é filha da User, então ela é chamada essa segunda vez para ser utilizada pela Customer.
