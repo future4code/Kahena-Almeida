f5f6189378e847023c83b54f22ae172c68f7b027
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-02-22 20:19:57 -0400

    Exercício 02

--- modulo5/criptografia-e-user-roles/src/app.ts
+++ modulo5/criptografia-e-user-roles/src/app.ts
@@ -1,20 +1,13 @@
 import express from 'express'
-import dotenv from 'dotenv'
-import { AddressInfo } from 'net'
-
-dotenv.config()
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo
-    console.log(`Server is running in http://localhost:${address.port}`)
-  } else {
-    console.error(`Failure upon starting server.`)
-  }
+app.listen(3003, () => {
+  console.log('Servidor rodando na porta 3003')
 })
 
-export default server
+export default app
--- modulo5/criptografia-e-user-roles/src/index.ts
+++ modulo5/criptografia-e-user-roles/src/index.ts
@@ -1,9 +1,4 @@
 import app from './app'
-import editUser from './endpoints/editUser'
 import createUser from './endpoints/createUser'
-import login from './endpoints/login'
-import { HashManager } from './services/HashManager'
 
 app.post('/users/signup', createUser)
-app.post('/users/login', login)
-app.put('/users', editUser)
