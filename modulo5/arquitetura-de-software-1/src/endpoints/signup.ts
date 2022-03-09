1cc8732266dff51578c22cd8b00d413df9cf21f4
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-03-08 20:07:46 -0400

    Finalizado

--- modulo5/arquitetura-de-software-1/src/endpoints/signup.ts
+++ modulo5/arquitetura-de-software-1/src/endpoints/signup.ts
@@ -1,52 +1,50 @@
-import { Request, Response } from "express";
-import { insertUser } from "../data/insertUser";
-import { generateToken } from "../services/authenticator";
-import { hash } from "../services/hashManager";
-import { generateId } from "../services/idGenerator";
+import { Request, Response } from 'express'
+import { insertUser } from '../data/insertUser'
+import { generateToken } from '../services/authenticator'
+import { hash } from '../services/hashManager'
+import { generateId } from '../services/idGenerator'
 
-export const signup = async (
-   req: Request,
-   res: Response
-) => {
-   try {
-      const { name, nickname, email, password, role } = req.body
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { name, nickname, email, password, role } = req.body
 
-      if (
-         !name ||
-         !nickname ||
-         !email ||
-         !password ||
-         !role
-      ) {
-         throw new Error('Preencha os campos "name","nickname", "email" e "password"')
-      }
+    if (!name || !nickname || !email || !password) {
+      throw new Error(
+        'Preencha os campos "name","nickname", "email" e "password"'
+      )
+    }
 
-      const id: string = generateId()
+    if (email.indexOf('@') === -1) {
+      throw new Error('Email inválido.')
+    }
 
-      const cypherPassword = await hash(password);
+    if (password.length < 6) {
+      throw new Error('Senha precisa ter no mínimo 6 dígitos.')
+    }
 
-      await insertUser({
-         id,
-         name,
-         nickname,
-         email,
-         password: cypherPassword,
-         role
-      })
+    const id: string = generateId()
 
-      const token: string = generateToken({
-         id,
-         role: role
-      })
+    const hashPassword = await hash(password)
 
-      res
-         .status(201)
-         .send({
-            message: "Usuário criado!",
-            token
-         })
+    await insertUser({
+      id,
+      name,
+      nickname,
+      email,
+      password: hashPassword,
+      role
+    })
 
-   } catch (error) {
-      res.status(400).send(error.message)
-   }
+    const token: string = generateToken({
+      id,
+      role: role
+    })
+
+    res.status(201).send({
+      message: 'Usuário criado!',
+      token
+    })
+  } catch (error: any) {
+    res.status(400).send(error.message)
+  }
 }
