e3ac95967c4602edd61d2f3285af48f666cd2903
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-03-22 11:02:35 -0400

    Início

---
+++ modulo6/promobit-front3/src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC1Eg27aKNkP3JUfAlr4sPSxAZMucc5ZFQ',
+  authDomain: 'promobit-03.firebaseapp.com',
+  projectId: 'promobit-03',
+  storageBucket: 'promobit-03.appspot.com',
+  messagingSenderId: '169948763080',
+  appId: '1:169948763080:web:66935c8add06aa3287a6fd'
+}
